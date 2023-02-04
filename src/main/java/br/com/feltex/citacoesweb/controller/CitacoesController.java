3efef734e9856d971756d026f1fa9a0c2c63323c
Rhaera <rauldantas@usp.br>
2023-02-04 17:46:46 -0300

    07

--- src/main/java/br/com/feltex/citacoesweb/controller/CitacoesController.java
+++ src/main/java/br/com/feltex/citacoesweb/controller/CitacoesController.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,12 @@ public class CitacoesController {
 
     public CitacoesController() throws IOException {
         carregarCitacoesDoArquivo();
+    }
+
+    @PostMapping("/citacao")
+    @Operation(summary = "Insira uma nova frase")
+    public void colocarCitacao(Citacao citacao) {
+        citacoes.add(citacao);        
     }
 
     @GetMapping("/citacoes")
